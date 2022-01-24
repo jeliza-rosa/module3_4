167a177cd5bfda107cf6608ef278a9b77f3156bf
alena_alekhina <pugofka08@mail.ru>
2022-01-24 12:41:04 +0500

    exercise4

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const string = 'Hello1!';
+
+export const data = {
+  message: 'Hello2!',
+};
+
+const dataTwo = {
+  alert: 'Hello3!',
+};
+
+export default dataTwo;
---
+++ src/function.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export function showAlert(x) {
+  alert(x);
+}
